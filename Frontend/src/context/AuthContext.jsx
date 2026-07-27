8c79077eaa239a22f36250590b6f301147e6042f
mahek1574 <mahekshanishvara5304@gmail.com>
2026-07-27 19:22:15 +0530

    upadetd with url

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -2,7 +2,8 @@ import { createContext, useState, useEffect, useContext } from "react";
 
 const AuthContext = createContext();
 
-const API_URL = "http://localhost:5000/api/auth";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_URL = `${BASE_URL}/api/auth`;
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
